4c12501a3fd48defd3308b7fee485db2aab05aed
vinaybeelagi <vinaykumar.b@neoito.com>
2024-01-03 17:52:25 +0530

    task1-DOM children

---
+++ documents.js
@@ -0,0 +1,9 @@
+// const newDiv = document.createElement('div');
+// newDiv.id = "dynamicDiv";
+// newDiv.innerText = "hello,this is added dynamically";
+// document.body.appendChild(newDiv) 
+
+// task-1
+console.log(document.body.firstElementChild)
+console.log(document.body.children[1])
+console.log(document.body.children[1].lastElementChild.innerHTML)
