ce7e295eb390187a2df587ca4b2dba1d226a6048
vinaybeelagi <vinaykumar.b@neoito.com>
2024-01-03 18:20:31 +0530

    task2 -The sibling question

--- documents.js
+++ documents.js
@@ -7,3 +7,12 @@
 console.log(document.body.firstElementChild)
 console.log(document.body.children[1])
 console.log(document.body.children[1].lastElementChild.innerHTML)
+
+// task2-The sibling question
+/*If elem – is an arbitrary DOM element node…
+
+Is it true that elem.lastChild.nextSibling is always null?
+Is it true that elem.children[0].previousSibling is always null ?
+solution
+Yes, true. The element elem.lastChild is always the last one, it has no nextSibling.
+No, wrong, because elem.children[0] is the first child among elements. But there may exist non-element nodes before it. So previousSibling may be a text node.*/
