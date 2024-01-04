1f88316779f5684c79249f8ad5a2cd687aeec024
vinaybeelagi <vinaykumar.b@neoito.com>
2024-01-04 13:16:11 +0530

    task-Count descendants

--- documents.js
+++ documents.js
@@ -50,3 +50,4 @@ alert( document.body instanceof HTMLElement ); // true
 alert( document.body instanceof Element ); // true
 alert( document.body instanceof Node ); // true
 alert( document.body instanceof EventTarget ); // true
+
